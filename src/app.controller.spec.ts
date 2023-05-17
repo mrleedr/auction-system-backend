a9e4feffba4ab37efc75231b96f50b8f3ec54469
mrleedr <lgdelrosario@outlook.com>
2023-05-17 19:00:53 +0800

    feat: test db

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EntityManager } from 'typeorm';
 
-describe('AppController', () => {
-  let appController: AppController;
+describe('AppService', () => {
+  let appService: AppService;
+  let entityManagerMock: Partial<EntityManager>;
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
+    entityManagerMock = {
+      query: jest.fn(() => Promise.resolve('Success') as any), // Mocking the query method to resolve with a string value
+    };
+
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        AppService,
+        { provide: EntityManager, useValue: entityManagerMock },
+      ],
     }).compile();
 
-    appController = app.get<AppController>(AppController);
+    appService = module.get<AppService>(AppService);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  describe('getHello', () => {
+    it('should return "Hello World!" if the query is successful', async () => {
+      const result = await appService.getHello();
+      expect(result).toEqual('Hello World!');
+      expect(entityManagerMock.query).toHaveBeenCalledWith('SELECT 1');
+    });
+
+    it('should return "Failed to connect to the database" if the query fails', async () => {
+      entityManagerMock.query = jest.fn(() =>
+        Promise.reject(new Error('Database connection error')),
+      );
+
+      const result = await appService.getHello();
+      expect(result).toEqual('Failed to connect to the database');
     });
   });
 });
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectEntityManager } from '@nestjs/typeorm';
+import { EntityManager } from 'typeorm';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Auction system api!';
+  constructor(@InjectEntityManager() private entityManager: EntityManager) {}
+
+  async getHello(): Promise<string> {
+    try {
+      // try to execute a simple SQL query
+      await this.entityManager.query('SELECT 1');
+      return 'Hello World!';
+    } catch (error) {
+      console.error(error);
+      return 'Failed to connect to the database';
+    }
   }
 }
